ec9e56391bedcec0b156b433da93b92ba9bfdbf0
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-01-30 17:38:55 +0100

    add triangle area - ex.8.4

--- js/promt_function.js
+++ js/promt_function.js
@@ -1,3 +1,9 @@
-var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name); // to jest komentarz liniowy wykonany zgodnie z treścia zadania 8.3
+var name=prompt('Wpisz swoje imię');
+var h, a, triangleArea;
+h=prompt('Podaj wysokość trójkata');
+a=prompt('Podaj długość podstawy');
+triangleArea = a*h/2;
+alert('Witaj, '+name); // to jest komentarz liniowy wykonany zgodnie z treścia zadania 8.3
 console.log('Witaj, ' + name);
+console.log("Pole trójkąta o podstawie a: "+a+" i wysokości h: "+h+" wynosi: "+triangleArea);
+
