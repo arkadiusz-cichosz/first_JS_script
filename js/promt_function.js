b041d4b8d5225af94a2441e787bf5356d9996c02
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-01-30 17:23:51 +0100

    add comment - ex. 8.3

--- js/promt_function.js
+++ js/promt_function.js
@@ -1,3 +1,3 @@
 var name = prompt('Wpisz swoje imię');
-alert('Witaj, ' + name);
+alert('Witaj, ' + name); // to jest komentarz liniowy wykonany zgodnie z treścia zadania 8.3
 console.log('Witaj, ' + name);
