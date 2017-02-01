ec4fb183fb84feba097a29c1001aa4fdd0a2ec0f
Arkadiusz Cichosz <cichorek87220@gmail.com>
2017-02-01 21:47:29 +0100

    fixed error

--- js/promt_function.js
+++ js/promt_function.js
@@ -5,25 +5,26 @@ console.log('Witaj, ' + name);
 //zadanie 8.4
 h = prompt('Podaj wysokość trójkata');
 a = prompt('Podaj długość podstawy');
-triangleArea = a*h/2;
+triangleArea = a * h / 2;
 console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
 //zadanie 8.5
 var b, c, value;
 b = prompt('Podaj wartość b');
 c = prompt('Podaj wartość c');
-value = (b*b) + (2*b*c) - (c*c);
+value = (b * b) + (2 * b * c) - (c * c);
 console.log(value);
 if (value > 0) {
 	console.log('wynik dodatni');
 }
 else if (value < 0) {
-	console.log('wynik ujemny')
+	console.log('wynik ujemny');
+} 
+else if (value == 0) {
+	console.log('wynik jest równy 0');
 }
-switch(value) {
-	case 0:
-		console.log('wynik jest równy 0');
-		break;
 
+else {
+	console.log('Wprowadzono nieprawidłowe dane');
 }
 
 
